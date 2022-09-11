d65dafc3cc31dde70b8147a9657b8217654c2157
mcsavvy <phishink5@gmail.com>
2022-09-12 00:33:14 +0100

    Add 102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,41 @@
+/* print combination of numbers */
+
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	/* first loop */
+	int first = 0, second;
+
+	while (first <= 99)
+	{
+		second = first;
+		while (second <= 99)
+		{
+			if (second != first)
+			{
+				putchar((first / 10) + 48);
+				putchar((first % 10) + 48);
+				putchar(' ');
+				putchar((second / 10) + 48);
+				putchar((second % 10) + 48);
+
+				if (first != 98 || second != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			second++;
+		}
+		first++;
+	}
+	putchar('\n');
+
+	return (0);
+}
