036149841c2af1b4f14cb1746ba30cb48e350634
mcsavvy <phishink5@gmail.com>
2022-09-15 19:52:21 +0100

    Fixed 3-print_remaining_days.c

---
+++ 0x03-debugging/3-convert_day.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+ * convert_day - converts day of month to day of year, without accounting
+ * for leap year
+ * @month: month in number format
+ * @day: day on the month
+ * Return: day of the year
+ */
+
+int convert_day(int month, int day)
+{
+	switch (month)
+	{
+		case 2:
+			day += 31;
+			break;
+		case 3:
+			day += 59;
+			break;
+		case 4:
+			day += 90;
+			break;
+		case 5:
+			day += 120;
+			break;
+		case 6:
+			day += 151;
+			break;
+		case 7:
+			day += 181;
+			break;
+		case 8:
+			day += 212;
+			break;
+		case 9:
+			day += 243;
+			break;
+		case 10:
+			day += 273;
+			break;
+		case 11:
+			day += 304;
+			break;
+		case 12:
+			day += 334;
+			break;
+		default:
+			break;
+	}
+	return (day);
+}
--- 0x03-debugging/README.md
+++ 0x03-debugging/README.md
@@ -12,3 +12,10 @@ This program stops an incoming Infinite loop \o/
 #### 2-largest_number
 Fixed a program that returns the largest of three integers
 
+#### 3-print_remaining_days.c
+Fixed a program that recieves the following arguments:
+* `month`: month of the year in number
+* `day`: the day of the year
+* `year`: year
+and prints the remaining days in the year _(taking leap years into account)_
+
