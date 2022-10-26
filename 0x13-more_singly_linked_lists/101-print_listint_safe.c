df2f57bf75b24839fef84a71e16d014ba1905c74
mcsavvy <phishink5@gmail.com>
2022-10-26 01:07:11 +0100

    Completed Task 12

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,71 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#define node listint_t
+
+/**
+ * find_loop - finds a loop in a list
+ * @head: the head of the list
+ * Return: the node where the loop begins, or NULL
+ *		if there is no loop
+ */
+node *find_loop(const node *head)
+{
+	node *slow, *fast;
+
+	if (!(head) || !(head->next))
+		return (NULL);
+	slow = head->next;
+	fast = slow->next;
+
+	while (fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+
+		if (slow == fast)
+		{
+			fast = head->next;
+			slow = slow->next;
+			while (slow != fast)
+			{
+				slow = slow->next;
+				fast = fast->next;
+			}
+			return (slow);
+		}
+	}
+	return (NULL);
+}
+
+/**
+ * print_listint_safe - prints all nodes in a linked list
+ * @head: the head of the list
+ * Return: the number of nodes in the list
+ */
+size_t print_listint_safe(const node *head)
+{
+	node *mark;
+	size_t size = 0;
+	char seen = 0;
+
+	mark = find_loop(head);
+	if (head == NULL)
+		return (size);
+
+	while (head != NULL)
+	{
+		printf("[%p] %d\n", (void *)head, head->n);
+		size++;
+		head = head->next;
+		if (head == mark)
+		{
+			if (seen)
+				break;
+			seen = 1;
+		}
+	}
+	if (mark != NULL)
+		printf("-> [%p] %d\n", (void *)mark, mark->n);
+	return (size);
+}
