79b7beafb4a7911269c800661cda59b775bcd6d4
mcsavvy <phishink5@gmail.com>
2023-04-13 00:26:43 +0100

    Completed tasks 0-6

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -33,10 +33,10 @@ int jump_search(int *array, size_t size, int value)
 	printf("Value found between indexes [%lu] and [%lu]\n", low, high);
 	high = MIN(high, size - 1);
 	for (; low < high && array[low] < value; low++)
-	    printf("Value checked array[%lu] = [%i]\n", low, array[low]);
+		printf("Value checked array[%lu] = [%i]\n", low, array[low]);
 	printf("Value checked array[%lu] = [%i]\n", low, array[low]);
 
 	if (array[low] == value)
-	    return (low);
+		return (low);
 	return (-1);
 }
